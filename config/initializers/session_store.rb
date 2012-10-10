ac02202a636240bb46e8b7b7045fb08983957d16
udaypeddireddy <udaypeddireddy@yahoo.com>
2012-10-10 17:40:59 +0530

    removed bapps

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Bapps::Application.config.session_store :cookie_store, key: '_bapps_session'
+Myblog::Application.config.session_store :cookie_store, key: '_myblog_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
