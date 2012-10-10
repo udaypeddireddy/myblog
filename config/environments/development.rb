34d12f2fd70e1ded46ea284607caa7edbc8d7be0
udaypeddireddy <udaypeddireddy@yahoo.com>
2012-10-10 16:55:03 +0530

    db up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-snip::Application.configure do
+Myblog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
