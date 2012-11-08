74bfbda3bb8840cf91bca18baed31d116fa99f7a
udaypeddireddy <udaypeddireddy@yahoo.com>
2012-11-08 13:08:31 +0530

    removed message from categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,7 +14,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1.json
   def show
     @category = Category.find(params[:id])
-    @message = "categories are show above"
+    
 
     respond_to do |format|
       format.html # show.html.erb
